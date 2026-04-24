43c3c896c2275e1ebfd97a6ba36e18e07dd027c7
Khushiitomarr <khushitomar301@gmail.com>
2026-04-25 03:15:01 +0530

    fix: move ChatMain into pages folder

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import App from "./App";
 import AIDetail from "./pages/AIDetail";
-import ChatMain from "./pages/ChatPage"
+import ChatMain from "./pages/ChatMain"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -15,7 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Route path="/" element={<App />} />
         <Route path="/ai" element={<AIDetail />} />
         <Route path="/search" element={<App />} />
-        <Route path="/chat" element={<ChatPage/>}/>
+        <Route path="/chat" element={<ChatMain/>}/>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
