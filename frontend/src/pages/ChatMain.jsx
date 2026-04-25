badcf2add4e9a6262c7049b343dede3cb052435d
Khushiitomarr <khushitomar301@gmail.com>
2026-04-25 07:48:30 +0530

    fix: correct ChatBot import path

--- frontend/src/pages/ChatMain.jsx
+++ frontend/src/pages/ChatMain.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import ChatBot from "./components/ChatBot";
+import ChatBot from "../components/ChatBot";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
