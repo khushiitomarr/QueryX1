79a4272adb59a6377269c85b85359d3d37a72981
Khushiitomarr <khushitomar301@gmail.com>
2026-04-25 07:52:53 +0530

    fix: force correct ChatBot casing

--- frontend/src/pages/ChatMain.jsx
+++ frontend/src/pages/ChatMain.jsx
@@ -1,12 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 import ChatBot from "../components/ChatBot";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+export default function ChatMain() {
+  return (
     <div className="min-h-screen bg-[#020617] flex items-center justify-center">
       <ChatBot />
     </div>
-  </React.StrictMode>
-);
+  );
+}
